c6139a9c10dbc6a0aa7599aaea76a4dc20c53f04
Torukuran <gineytor12@hotmail.com>
2022-11-04 08:53:08 -0500

    Modulo 4

--- src/controllers/persona.controller.ts
+++ src/controllers/persona.controller.ts
@@ -1,3 +1,4 @@
+import { service } from '@loopback/core';
 import {
   Count,
   CountSchema,
@@ -16,15 +17,47 @@ import {
   del,
   requestBody,
   response,
+  HttpErrors,
 } from '@loopback/rest';
-import {Persona} from '../models';
+import {Credenciales, Persona} from '../models';
 import {PersonaRepository} from '../repositories';
+import { AutenticacionService } from '../services';
+const fetch = require('node-fetch');
 
 export class PersonaController {
   constructor(
     @repository(PersonaRepository)
     public personaRepository : PersonaRepository,
+    @service(AutenticacionService)
+    public servicioAutenticacion : AutenticacionService
   ) {}
+
+  @post('/identificarPersona', {
+    responses:{
+      '200':{
+        description:'Identificacion de usuario'
+      }
+    }
+  })
+  async identificarPersona(
+    @requestBody() credenciales: Credenciales
+  ){
+    let p = await this.servicioAutenticacion.IdentificarPersona(credenciales.Usuario, credenciales.clave);
+    if(p){
+      let token = this.servicioAutenticacion.GenerarTokenJWT(p);
+      return{
+        datos:{
+          nombre: p.Nombres,
+          correo: p.correo,
+          id:p.Id
+        },
+        tk: token
+      }
+    }else{
+      throw new HttpErrors[401]('Datos invalidos');
+    }
+
+  }
 
   @post('/personas')
   @response(200, {
@@ -44,7 +77,21 @@ export class PersonaController {
     })
     persona: Omit<Persona, 'id'>,
   ): Promise<Persona> {
-    return this.personaRepository.create(persona);
+    let clave = this.servicioAutenticacion.GenerarClave();
+    let claveCifrada = this.servicioAutenticacion.CifrarClave(clave);
+    persona.clave = claveCifrada;
+    let p = await this.personaRepository.create(persona);
+
+    //Notificar al usuario
+    let destino = persona.correo;
+    let asunto = 'Registro en la plataforma';
+    let contenido = `Hola ${persona.Nombres} su usuario es ${persona.correo} y su contraseña es ${clave}`;
+    fetch(`http://127.0.0.1:5000/email?correo_destino=${destino}&asunto=${asunto}&contenido=${contenido}`)
+      .then((data:any)=>{
+        console.log(data);
+      })
+    return p;
+
   }
 
   @get('/personas/count')
--- src/datasources/mongodb.datasource.ts
+++ src/datasources/mongodb.datasource.ts
@@ -4,7 +4,7 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'mongodb',
   connector: 'mongodb',
-  url: 'mongodb+srv://ginaR:kushina12@cluster0.enoydb1.mongodb.net/Pedidos?retryWrites=true&w=majority',
+  url: 'mongodb+srv://ginaR:Kushina12@cluster0.enoydb1.mongodb.net/Pedidos?retryWrites=true&w=majority',
   host: '',
   port: 0,
   user: '',
